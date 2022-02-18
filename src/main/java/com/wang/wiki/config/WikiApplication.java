1ca9f52ae492828118e6f1fced62cfc3fc179397
Wang <wangxinyuanchn@163.com>
2022-02-18 19:20:03 +0800

    移动启动文件

--- src/main/java/com/wang/wiki/config/WikiApplication.java
+++ src/main/java/com/wang/wiki/config/WikiApplication.java
@@ -1,15 +1,17 @@
-package com.wang.wiki;
+package com.wang.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 /**
  * @author Wang
  * @Overview 启动文件
  */
+@ComponentScan("com.wang")
 @SpringBootApplication
 public class WikiApplication {
 
