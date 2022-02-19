a2309db79433213fd89af527cc0947e63003ad32
Wang <wangxinyuanchn@163.com>
2022-02-19 16:42:29 +0800

    粗略制作显示页面

--- src/main/java/com/wang/wiki/ebook/service/EbookService.java
+++ src/main/java/com/wang/wiki/ebook/service/EbookService.java
@@ -6,6 +6,7 @@ import com.wang.wiki.ebook.mapper.EbookMapper;
 import com.wang.wiki.ebook.vo.EbookVO;
 import com.wang.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -21,12 +22,15 @@ public class EbookService {
 
     /**
      * 电子书列表查询
+     *
      * @param ebook 查询条件
      * @return 查询结果
      */
-    public List<EbookVO> list(EbookVO ebook){
+    public List<EbookVO> list(EbookVO ebook) {
         QueryWrapper<EbookEntity> wrapper = new QueryWrapper<>();
-        wrapper.like("c_name", ebook.getName());
+        if (!ObjectUtils.isEmpty(ebook.getName())) {
+            wrapper.like("c_name", ebook.getName());
+        }
         List<EbookEntity> list = ebookMapper.selectList(wrapper);
         return CopyUtil.copyList(list, EbookVO.class);
     }
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -48,24 +48,70 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      Content
+      <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+        <template #renderItem="{ item }">
+          <a-list-item key="item.name">
+            <template #actions>
+              <span v-for="{ type, text } in actions" :key="type">
+                <component v-bind:is="type" style="margin-right: 8px" />
+                {{ text }}
+              </span>
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.name }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.cover" /></template>
+            </a-list-item-meta>
+          </a-list-item>
+        </template>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
+import {defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
 
 export default defineComponent({
   name: 'HomeView',
+  components: {
+    StarOutlined,
+    LikeOutlined,
+    MessageOutlined,
+  },
   setup() {
-    console.log("setup");
-    axios.get("http://localhost/ebook/list?name=My").then((response) => {
-      console.log(response);
-    })
+    const ebooks = ref();
+
+    onMounted(() =>{
+      axios.get("/ebook/list").then((response) => {
+        const data = response.data;
+        ebooks.value = data.content;
+      })
+    });
+
+    const actions: Record<string, string>[] = [
+      { type: 'StarOutlined', text: '156' },
+      { type: 'LikeOutlined', text: '156' },
+      { type: 'MessageOutlined', text: '2' },
+    ];
+
+    return {
+      ebooks,
+      actions,
+    }
   }
 });
 </script>
 
-
+<style scoped>
+.ant-avatar {
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 8%;
+  margin: 5px 0;
+}
+</style>
